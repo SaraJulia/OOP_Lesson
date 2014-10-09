f86a72377f40e65ee686243e0e6637c34df2272e
Hackbright Student <info@hackbrightacademy.com>
2014-10-08 17:09:03 -0700

    Prints out directions when direction arrows are pressed.

--- game.py
+++ game.py
@@ -15,6 +15,26 @@ GAME_HEIGHT = 5
 #### Put class definitions here ####
 class Rock(GameElement):
     IMAGE = "Rock"
+
+
+class Character(GameElement):
+    IMAGE = "Princess"
+
+    def keyboard_handler(self, symbol, modifier):
+        if symbol == key.UP:
+            self.board.draw_msg('%s says: "You pressed up!"' % self.IMAGE)
+        elif symbol == key.DOWN:
+            self.board.draw_msg('%s says: "You pressed down!"' % self.IMAGE)
+        elif symbol == key.LEFT:
+            self.board.draw_msg('%s says: "You pressed left!"' % self.IMAGE)
+        elif symbol == key.RIGHT:
+            self.board.draw_msg('%s says: "You pressed right!"' % self.IMAGE)
+        elif symbol == key.SPACE:
+            self.board.erase_msg()
+
+
+
+
 ####   End class definitions    ####
 
 def initialize():
@@ -37,3 +57,11 @@ def initialize():
 
     for rock in rocks:
         print rock
+
+
+    player = Character()
+    GAME_BOARD.register(player)
+    GAME_BOARD.set_el(2, 2, player)
+    print player
+
+    GAME_BOARD.draw_msg("This game is wicked awesome. Because we're Bostonites")
