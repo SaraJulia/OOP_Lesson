96b1e4608c98279b07786cd813f2371e20e8c057
Hackbright Student <info@hackbrightacademy.com>
2014-10-10 13:07:18 -0700

    message print correctly. bug will steal gems when interact is called.

--- game.py
+++ game.py
@@ -14,42 +14,52 @@ GAME_HEIGHT = 8
 
 #### Put class definitions here ####
 class Rock(GameElement):
+    # Rocks are solid by default and have the image of rock
     IMAGE = "Rock"
     SOLID = True
 
-    # def interact(self, player):
-    #     pass
-
 class Wall(GameElement):
+    # Walls are solid by defailt and have the image of a low wal
     IMAGE = "Wall"
     SOLID = True
 
 class Key(GameElement):
+    # keys have the key image and a the name of key and are not solid.
+    # interact method allows keys to be picked up by player
     IMAGE = "Key"
     SOLID = False
     NAME = 'key'
 
     def interact(self, player):
+        #adds the name of the key to the player's inventory
         player.inventory.append(self.NAME)
         GAME_BOARD.draw_msg("You just acquired the KEY! You have %d items!" %(len(player.inventory)))
         print player.inventory
 
 class DoorClosed(GameElement):
+    #doors have the image of a closed door and is solid by default.
+    #after player gets the key the door finds the key, changes the image, and becomes not solid
     IMAGE = "DoorClosed"
     SOLID = True
+    message = False
 
     def interact(self, player):
         if 'key' in player.inventory and self.IMAGE == 'DoorClosed':
             self.change_image('DoorOpen')
-            #self.SOLID = False 
-            GAME_BOARD.draw_msg("HELLYA, DOOR IS OPEN!!!") 
+            GAME_BOARD.draw_msg("HELLYA, DOOR IS OPEN!!!")
+            self.message = True 
+            #print message
         elif self.IMAGE == 'DoorOpen':
             self.SOLID = False
         else:
+            self.message = True
+            #print message
             GAME_BOARD.draw_msg("GO GET THE KEY!!!!!!!")
 
 class Gem(GameElement):
-    #IMAGE = "BlueGem"
+    #gems are not solid and are added to player inventory 
+    #gems can be in 3 colors: blue, green, orange, and the gem color is determined
+    #by the choose color method
     IMAGE = ''
     SOLID = False
     NAME = "gem"
@@ -65,17 +75,22 @@ class Gem(GameElement):
     def interact(self, player):
         player.inventory.append(self.NAME)
         GAME_BOARD.draw_msg("You just acquired a gem! You have %d items!" %(len(player.inventory)))
+        print player.inventory
 
-class BlueGem(Gem):
-    IMAGE = "BlueGem"
+# this is another way to choose color, maybe we can refactor to include subclasses later
+# class BlueGem(Gem):
+#     IMAGE = "BlueGem"
 
 
 class Chest_Closed(GameElement):
+    #the closed chest works just like the door. When it opens, it reveals the star
+    #which ends the game.
     IMAGE = "Chest"
     SOLID = True
-
+    message = False
 
     def interact(self, player):
+        self.message = True
         if 'gem' in player.inventory and self.IMAGE == 'Chest':
             self.change_image('Star') 
             GAME_BOARD.draw_msg("Get the Star and finish the game!!!") 
@@ -86,8 +101,10 @@ class Chest_Closed(GameElement):
             GAME_BOARD.draw_msg("You don't have enough gems!!")
 
 class EnemyBug(GameElement):
+    # enemybug is a non player character which crawls across the middle of the board.
     IMAGE = 'EnemyBug'
     direction = 1
+    message = False
 
     def update(self, dt):
 
@@ -97,11 +114,31 @@ class EnemyBug(GameElement):
             self.direction *= -1
             next_x = self.x
 
-        self.board.del_el(self.x, self.y)
-        self.board.set_el(next_x, self.y, self)
+        existing_el = self.board.get_el(next_x, self.y)
+        if existing_el:
+            print existing_el
+            #self.interact(existing_el)
+        if not existing_el or existing_el.SOLID == False:
+            self.board.del_el(self.x, self.y)
+            self.board.set_el(next_x, self.y, self)
+
+    def interact(self, player):
+
+        if 'gem' in player.inventory:
+            self.message = True
+            player.inventory.remove('gem')
+            GAME_BOARD.draw_msg('The bug stole your gem! Sux for you.')
+            self.direction *= -1
+            #debugging code:
+            print player.inventory
+        else:
+            GAME_BOARD.draw_msg('The bug ate you! GAME OVER!')
+            self.message = True
+            
 
 class Character(GameElement):
     IMAGE = "Princess"
+    SOLID = False 
 
     def next_pos(self, direction):
         if direction == 'up':
@@ -157,10 +194,20 @@ class Character(GameElement):
 
                 if existing_el:
                     existing_el.interact(self)
-                
+
+                #debugging code:
+
+                # if existing_el.message:
+                #     print existing_el.message
 
                 if existing_el and existing_el.SOLID:
-                    self.board.draw_msg("There's something in my way!")
+                    if hasattr(existing_el, "message"):
+                        if existing_el.message == True:
+                            pass
+                    else:
+                        self.board.draw_msg("There's something in my way!")
+         
+
                 elif existing_el is None or not existing_el.SOLID:
                     self.board.del_el(self.x, self.y)
                     self.board.set_el(next_x, next_y, self)
